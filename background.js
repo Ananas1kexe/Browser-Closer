0104a700d45601c5d4d02b386de8ec645514247c
ananas1k <sashafarberov2010@gmail.com>
2026-01-18 17:46:45 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "close_all") {
-        // Получаем все окна браузера
         chrome.windows.getAll({}, (windows) => {
             windows.forEach((window) => {
                 chrome.windows.remove(window.id);
